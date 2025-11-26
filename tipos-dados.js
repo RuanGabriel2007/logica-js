3f12e9f0610ba9360a6249cddfe75b8c9c1a919b
RUAN GABRIEL DE ANDRADE LOPES <RUANLOPES0962@digitalcollege.internal>
2025-11-25 21:46:28 -0300

    first commit

---
+++ tipos-dados.js
@@ -0,0 +1,146 @@
+// // tipos de texto- sting
+// // "Isso é um texto rapa";
+// // "Ruan";
+// // 'isso tambem é um "texto" mesmo com aspas simples';
+// // `usar a crase o js entende como string`;
+
+// // number
+// 100 //inteiros;
+// 25.35 //decimais- float;
+
+// // tipo booleano - boolean - true / false
+// true
+// false
+
+// //tipo indefinido - undefined
+// undefined
+
+// //tipo nulo - null
+// null
+
+// //O que é uma variável?
+// // um espaço na mémoria do computador onde podemos armazenar dados para serem utilizados posteriomente no código
+
+// var meuSobrenome = "Lopes";
+// let caraDaCamisaVinho = "Ruan";
+// const altura = 1.90;
+// //padrao de escrita chamada CamelCase   
+
+// console.log(caraDaCamisaVinho);
+
+// caraDaCamisaVinho = "Anderson";
+
+// console.log(caraDaCamisaVinho);
+
+// let temCarro = true;
+// let isOpen= false;
+
+// let profissao = undefined;
+
+// const meuNulo = null;
+
+// console.log (profissao);
+// console.log(meuNulo);
+
+let contador= "0"
+let mensagem = "Ola javascript"
+let nomeDoCarro = "Mobi"
+
+console.log(contador);
+console.log(mensagem);
+console.log(nomeDoCarro);
+
+contador =  "1"
+console.log(contador);
+
+contador =  "2"
+console.log(contador);
+
+mensagem = "Pivete é sal"
+
+console.log(mensagem);
+
+nomeDoCarro = "quadrado"
+
+
+const cidade = "Fortaleza";
+const aprovado = true;
+const melhorTimeDoMundo = "Fortaleza";
+
+console.log(cidade);
+console.log(aprovado);
+console.log(melhorTimeDoMundo);
+
+// operações matemáticas 
+
+let numero1= 20;
+let numero2= 10;
+
+let soma = numero1 + numero2;
+console.log(soma);
+
+ soma = 50
+numero2 = 5;
+
+console.log(soma);
+
+let resultSubtracao = soma - numero2;
+
+console.log(resultSubtracao);
+
+let result3 = 30 * 3;
+
+console.log(result3);
+
+let result4 = 50 / 10 
+
+console.log(result4)
+
+//mod - resto da divisao 
+
+let result5 = 10 % 3
+
+console.log(result5)
+
+15/100
+
+// verificando no console os tipos de dados - typeof
+
+console.log(typeof result5)
+
+let numero6 = 40;
+let numero7 = 20;
+
+let mais = numero6 + numero7;
+
+console.log(mais);
+ 
+soma = 70
+numero8 = 20 
+
+let subtraifi = soma - numero8;
+ console.log(subtraifi)
+
+
+ soma = 100 
+ numero9 = 5 
+ let multiplicaçao = soma * numero9;
+ console.log(multiplicaçao)
+ 
+ soma = 80
+ numero10 = 4
+ let divisao = soma / numero10;
+ console.log(divisao)
+
+soma = 200
+numero11 = 6
+let resto = soma % numero11;
+console.log(resto)
+
+console.log(typeof mais);
+console.log(typeof subtraifi);
+console.log(typeof multiplicaçao);
+console.log(typeof divisao);
+console.log(typeof resto);
+
+
